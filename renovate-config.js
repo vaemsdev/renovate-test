0bb29458c50ffc05b52ebde65c2c45e7f3ec3454
VAEMSDEV <vaemsdev@gmail.com>
2023-03-10 11:10:50 +0530

    init

---
+++ renovate-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branchPrefix: 'vd-renovate/',
+    dryRun: true,
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    logLevel: 'debug',
+    onboarding: false,
+    platform: 'github',
+    repositories: ['vaemsdev/renovate-test'],
+};
