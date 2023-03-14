9df7f71c47cd500a7810f7c999741d3634674836
VAEMSDEV <vaemsdev@gmail.com>
2023-03-14 10:04:17 +0530

    Update config.js

--- .github/config.js
+++ .github/config.js
@@ -4,4 +4,21 @@ module.exports = {
     onboarding: false,
     platform: 'github',
     repositories: ['vaemsdev/renovate-test'],
+    packageRules: {
+      "groupName": "all dependencies",
+      "groupSlug": "all",
+      "lockFileMaintenance": {
+        "enabled": false
+      },
+      "packageRules": [
+        {
+          "groupName": "all dependencies",
+          "groupSlug": "all",
+          "matchPackagePatterns": [
+            "*"
+          ]
+        }
+      ],
+      "separateMajorMinor": false
+    }
 };
